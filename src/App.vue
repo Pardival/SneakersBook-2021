8c8f392bbfe3a516d5e2fece40a1fea9fd2ae50d
Pardival <kevin.dufourpro@gmail.com>
2021-02-08 20:53:36 +0100

    Creation du Header et des SneakersCard

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <Header/>
+
+    <v-main>
+      <v-row>
+        <v-col><SneakersCard/></v-col>
+        <v-col><SneakersCard/></v-col>
+        <v-col><SneakersCard/></v-col>
+        <v-col><SneakersCard/></v-col>
+    </v-row>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SneakersCard from './components/SneakersCard';
+import Header from './components/Header';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    SneakersCard,
+    Header
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/SneakersCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-card elevation="3"
+            outlined>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                   height="200px" class="mx-auto" max-width="344"></v-img>
+        <v-card-title>Nom Sneakers</v-card-title>
+
+        <v-card-subtitle>
+        1,000 miles of wonder
+      </v-card-subtitle>
+    </v-card>
+</template>
